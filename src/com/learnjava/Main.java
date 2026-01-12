8b7a6ca0e9ae8cd5e5ff955f6634b3206dc150dd
Nagaraju Brahmanapally <nb114@students.uwf.edu>
2026-01-11 21:04:52 -0600

    removed comment lines

--- src/com/learnjava/Main.java
+++ src/com/learnjava/Main.java
@@ -3,8 +3,6 @@ package com.learnjava;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
